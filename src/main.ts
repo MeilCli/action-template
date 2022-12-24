d059f092ae225c9d929935237af3ad40aebd9f5e
MeilCli <ics.sora.meil@gmail.com>
2022-12-24 21:49:20 +0900

    test action-CI

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { getOption } from "./option";
 async function run() {
     try {
         const option = getOption();
+        core.info("test");
     } catch (error) {
         if (error instanceof Error) {
             core.setFailed(error.message);
