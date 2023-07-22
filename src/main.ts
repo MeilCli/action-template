9b8d2e0a6702eabbad4862d346cb8c4b390516af
MeilCli <ics.sora.meil@gmail.com>
2023-07-22 15:47:12 +0900

    dependency: update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { getOption } from "./option";
 
 async function run() {
     try {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const option = getOption();
     } catch (error) {
         if (error instanceof Error) {
